0e57dc1454e807dc49dfbe0188d0a575e9e6aeaf
Ryan Pinnock <ryanpinnock10@gmail.com>
2019-10-04 02:55:10 -0400

    Updated version, currency conversion attempted. TODO: Give app a cover and add settings view

--- tippar2/ViewController.swift
+++ tippar2/ViewController.swift
@@ -10,11 +10,50 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var billLabel: UITextField!
+    
+    @IBOutlet weak var tipLabel: UILabel!
+    
+    @IBOutlet weak var totalLabel: UILabel!
+    
+    @IBOutlet weak var tipControl: UISegmentedControl!
+    
+    @IBOutlet weak var currencyControl: UISegmentedControl!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    @IBAction func dissapearGesture(_ sender: Any) {
+        view.endEditing(true)
+    }
+    @IBAction func calculateTip(_ sender: Any) {
+        //Change currency
+        
+//        let currencyFormatter = NumberFormatter()
+//        currencyFormatter.usesGroupingSeparator = true
+//        currencyFormatter.numberStyle = .currency
+//        // localize to your grouping and decimal separator
+//        currencyFormatter.locale = Locale.current
+        
+        //Get the bill amount
+        let bill = Double(billLabel.text!) ?? 0
+        
+        //Calculate the tip and total
+        let tipPercentages = [0.15, 0.18, 0.2]
+//        let currencyChanged = ["£", "€", "¥", "$"]
 
-
+//        let currency = currencyChanged[currencyControl.selectedSegmentIndex]
+        let tip = bill * tipPercentages[tipControl.selectedSegmentIndex]
+        let total = bill + tip
+            
+        //Update the tip amount
+        tipLabel.text = String(format:  "$%.2f", tip)
+        totalLabel.text = String(format:  "$%.2f", total)
+        //String(format: "%.2f", total)
+        
+    }
+    
 }
 

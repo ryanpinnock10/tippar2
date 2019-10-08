2a8309ba14cc5b98d3ac214180ee5e19eb81ef9f
Ryan Pinnock <ryanpinnock10@gmail.com>
2019-10-07 22:45:42 -0400

    Settings section is under construction. This section includes: currency change, a tip or no tip option and a custom tip option.

--- tippar2/ViewController.swift
+++ tippar2/ViewController.swift
@@ -20,6 +20,9 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var currencyControl: UISegmentedControl!
     
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
---
+++ tippar2/settingsViewController.swift
@@ -0,0 +1,48 @@
+//
+//  settingsViewController.swift
+//  tippar2
+//
+//  Created by Ryan Pinnock on 10/7/19.
+//  Copyright © 2019 Ryan Pinnock. All rights reserved.
+//
+
+import UIKit
+
+class settingsViewController: UIViewController {
+
+    @IBOutlet weak var tipOrNoTip: UISegmentedControl!
+    
+    @IBOutlet weak var customTip: UISegmentedControl!
+    
+    @IBOutlet weak var currencyChange: UISegmentedControl!
+    
+    @IBOutlet weak var darkModeSwitch: UISwitch!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func endSettingsEditing(_ sender: Any) {
+        view.endEditing(true)
+    }
+    
+    @IBAction func changeMode(_ sender: Any) {
+    }
+    
+    @IBAction func tipOrNone(_ sender: Any) {
+    }
+    
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
